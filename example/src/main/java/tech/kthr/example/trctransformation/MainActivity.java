9c54d67af80866309c615e213ef6e9a18398d415
kazuyakitahara <kitahara.kazuya@gmail.com>
2016-04-29 02:37:52 +0900

    Use normal Transformation

--- example/src/main/java/tech/kthr/example/trctransformation/MainActivity.java
+++ example/src/main/java/tech/kthr/example/trctransformation/MainActivity.java
@@ -10,15 +10,17 @@ import tech.kthr.example.trctransformation.databinding.ActivityMainBinding;
 import tech.kthr.trctransformation.BottomRoundedCornerTransformation;
 import tech.kthr.trctransformation.LeftRoundedCornerTransformation;
 import tech.kthr.trctransformation.RightRoundedCornerTransformation;
+import tech.kthr.trctransformation.RoundedCornerTransformation;
 import tech.kthr.trctransformation.TopRoundedCornerTransformation;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding = null;
-    private static final String SAMPLE_URL_1 = "https://www.pakutaso.com/assets_c/2016/02/KAZ7842_P1110102-thumb-autox1500-20691.jpg";
-    private static final String SAMPLE_URL_2 = "https://www.pakutaso.com/shared/img/thumb/PAK88_tsurareneko20150109081309_TP_V.jpg";
-    private static final String SAMPLE_URL_3 = "https://www.pakutaso.com/shared/img/thumb/HIRA88_hikkurikaetteniramiwokikasu20130910001351_TP_V.jpg";
-    private static final String SAMPLE_URL_4 = "https://www.pakutaso.com/shared/img/thumb/HIRA88_nihikisorottesc20140809134818_TP_V.jpg";
-    private static final String SAMPLE_URL_5 = "https://www.pakutaso.com/assets_c/2016/03/P1090145-3-thumb-autox1000-21374.jpg";
+    private static final String IMAGE_URL_NORMAL = "https://www.pakutaso.com/shared/img/thumb/P1080011_TP_V.jpg";
+    private static final String IMAGE_URL_TOP = "https://www.pakutaso.com/assets_c/2016/02/KAZ7842_P1110102-thumb-autox1500-20691.jpg";
+    private static final String IMAGE_URL_RIGHT = "https://www.pakutaso.com/shared/img/thumb/PAK88_tsurareneko20150109081309_TP_V.jpg";
+    private static final String IMAGE_URL_BOTTOM = "https://www.pakutaso.com/shared/img/thumb/HIRA88_hikkurikaetteniramiwokikasu20130910001351_TP_V.jpg";
+    private static final String IMAGE_URL_LEFT = "https://www.pakutaso.com/shared/img/thumb/HIRA88_nihikisorottesc20140809134818_TP_V.jpg";
+    private static final String IMAGE_URL_COMPOSE = "https://www.pakutaso.com/assets_c/2016/03/P1090145-3-thumb-autox1000-21374.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +31,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void setSampleImages() {
         int cornerRadiusPx = getResources().getDimensionPixelSize(R.dimen.corner_radius);
-        Picasso.with(this).load(SAMPLE_URL_1)
+        Picasso.with(this).load(IMAGE_URL_NORMAL)
+                .fit()
+                .centerCrop()
+                .transform(new RoundedCornerTransformation(cornerRadiusPx, 0))
+                .into(this.binding.normalRoundedCornerImageView);
+
+        Picasso.with(this).load(IMAGE_URL_TOP)
                 .fit()
                 .centerCrop()
                 .transform(new TopRoundedCornerTransformation(cornerRadiusPx, 0))
                 .into(this.binding.topRoundedCornerImageView);
 
-        Picasso.with(this).load(SAMPLE_URL_2)
+        Picasso.with(this).load(IMAGE_URL_RIGHT)
                 .fit()
                 .centerCrop()
                 .transform(new RightRoundedCornerTransformation(cornerRadiusPx, 0))
                 .into(this.binding.rightRoundedCornerImageView);
 
-        Picasso.with(this).load(SAMPLE_URL_3)
+        Picasso.with(this).load(IMAGE_URL_BOTTOM)
                 .fit()
                 .centerCrop()
                 .transform(new BottomRoundedCornerTransformation(cornerRadiusPx, 0))
                 .into(this.binding.bottomRoundedCornerImageView);
 
-        Picasso.with(this).load(SAMPLE_URL_4)
+        Picasso.with(this).load(IMAGE_URL_LEFT)
                 .fit()
                 .centerCrop()
                 .transform(new LeftRoundedCornerTransformation(cornerRadiusPx, 0))
                 .into(this.binding.leftRoundedCornerImageView);
 
-        Picasso.with(this).load(SAMPLE_URL_5)
+        Picasso.with(this).load(IMAGE_URL_COMPOSE)
                 .fit()
                 .centerCrop()
                 .transform(new TopRoundedCornerTransformation(cornerRadiusPx, 0))
